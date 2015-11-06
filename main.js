f9615666afe6f4b604364d8f50bf5d4d83a1e05e
Cassie Moy <cassiemoy@positiondev.com>
2015-11-06 17:51:23 -0500

    EOD commit - begin refactoring JS, add 'You are here' icon.

--- main.js
+++ main.js
@@ -1,43 +1,94 @@
-var data;
-var index = 0;
+$(document).ready(function() {
 
-var frame = d3.select('#map');
-var preview = d3.select('#preview');
-var quote = d3.select('#quote');
-var context = d3.select('#context');
-var source = d3.select('#source');
-var address = d3.select('#location');
+  var data;
+  var tour = "Learning to Read New York";
+  var index = 0;
+  var hash = window.location.hash;
 
-var hash = window.location.hash;
-
-d3.csv('data.csv', function(_data) {
-  loadTour(_data, 'Learning to Read New York');
-
-  $('#forward').on('click', function(e) {
-    nextPlace();
+  $(document).on('change', '#select-tour', function(){
+    console.log($('#select-tour').val());
+    console.log("here is location:" + hash.substring(1));
   });
 
-  $('#backward').on('click', function(e) {
-    prevPlace();
-  });
+  d3.csv('data.csv', function(data) {
+    var tourData = loadTour(data, tour);
 
-  $('#read-more').on('click', function(e) {
-    expand();
-  });
+    $('#forward').on('click', function(e) { nextPlace(); });
+    $('#backward').on('click', function(e) { prevPlace(); });
+    $('#read-more').on('click', function(e) { expand(); });
 
-  $(window).on('hashchange', function() {
+    $(window).on('hashchange', function() {
+      go(window.location.hash.substring(1));
+    });
+
     go(window.location.hash.substring(1));
   });
 
-  go(window.location.hash.substring(1));
+  initializeMap();
+
+  function prevPlace() {
+    index --;
+    if (index < 0) {
+      index = tourData.length - 1;
+    }
+    window.location.hash = '#' + index;
+  }
+
+  function nextPlace() {
+    index ++;
+    if (index > tourData.length - 1) {
+      index = 0;
+    }
+    window.location.hash = '#' + index;
+  }
 });
 
-function loadTour(_data, theme) {
-  data = _data.filter(function(d) {
-    return d['Theme'] == theme;
+function initializeMap() {
+  var ctrLongitude = 40.759081;
+  var ctrLatitude = -73.978492;
+
+  var redCircleIcon = L.icon({ iconUrl: 'images/capital-red-circle-map.png',
+                               iconSize: [18, 18]})
+  var hereIcon = L.icon({ iconUrl: 'images/capital-walker.png',
+                          iconSize: [100, 100]})
+
+
+  var tourMap = L.map('tour-map').setView([ctrLongitude, ctrLatitude], 13);
+
+  L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+  }).addTo(tourMap);
+
+  L.marker([ctrLongitude, ctrLatitude], {icon: hereIcon, zIndexOffset: 1000}).addTo(tourMap);    
+
+  d3.csv("data.csv", function (data) {
+    var index = 0;
+    data.map(function (d) {
+      if (d['Streetview Embed Code']) {
+        var lat = parseFloat(d['Streetview Embed Code'].match(/1d(.*?)!/)[1]);
+        var lng = parseFloat(d['Streetview Embed Code'].match(/2d(.*?)!/)[1]);
+        d['Index'] = index;
+
+        if (lat && lng) {
+          var marker = L.marker([lat, lng], {icon: redCircleIcon}).addTo(tourMap);
+          marker.on('click', function() { go(d['Index'])});
+        }     
+    
+      }
+      index += 1;
+    });
+  });
+}
+
+function loadTour(data, tour) {
+  var tourIndex = 0;
+
+  tourData = data.filter(function(d) {
+    console.log(reformat(d['Tour']));
+    return d['Tour'] == tour;
   });
 
-  data = data.map(function(d) {
+  tourData = tourData.map(function(d) {
     d.embed = d['Streetview Embed Code'].match(/src="(.*?)"/)[1];
     d.lat = d['Streetview Embed Code'].match(/1d(.*?)!/)[1];
     d.lng = d['Streetview Embed Code'].match(/2d(.*?)!/)[1];
@@ -45,48 +96,41 @@ function loadTour(_data, theme) {
     d['Preview'] = d['Preview'].replace('\n', '<br>');
     d['Quote'] = d['Quote'].replace('\n', '<br>');
     d['Source'] = d['Source'].replace('\n', '<br>');
-
     return d;
   });
-}
 
-function prevPlace() {
-  index --;
-  if (index < 0) {
-    index = data.length - 1;
-  }
-  window.location.hash = '#' + index;
-}
-
-function nextPlace() {
-  index ++;
-  if (index > data.length - 1) {
-    index = 0;
-  }
-  window.location.hash = '#' + index;
+  return tourData;
 }
 
 function go(i) {
   if (typeof i != 'number') i = +i;
-
   index = i;
-  frame.attr('src', data[index].embed);
-  quote.html(data[index]['Quote']);
-  preview.html(data[index]['Preview']);
-  context.html(data[index]['Context']);
-  source.html(data[index]['Source']);
-  address.text(data[index]['Location']);
+  
+  $("#map").attr('src', tourData[index].embed);
+  $("#quote").html(tourData[index]['Quote']);
+  $("#preview").html(tourData[index]['Preview']);
+  $("#context").html(tourData[index]['Context']);
+  $("#source").html(tourData[index]['Source']);
+  $("#location").html(tourData[index]['Location']);
+  
   window.location.hash = '#' + index;
 }
 
 function expand() {
-  if ($("#read-more").html() == "Read More") {
-    $("#preview").hide();
-    $("#quote").show();
-    $("#read-more").html("Read Less");
-  } else {
-    $("#quote").hide();
-    $("#preview").show();
-    $("#read-more").html("Read More");
-  }
+  if ($("#quote").text() != "") {
+    $("#read-more").show();
+    if ($("#read-more").html() == "Read More") {
+      $("#preview").hide();
+      $("#quote").show();
+      $("#read-more").html("Read Less");
+    } else {
+      $("#quote").hide();
+      $("#preview").show();
+      $("#read-more").html("Read More");
+    }
+  } 
+}
+
+function reformat(title) {
+  return title.replace(/\s+/g, '-').toLowerCase();
 }
