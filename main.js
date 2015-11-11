f079a87dab97276a353ff9f10f32483d5643ca9e
Matt J Raibert <mattraibert@positiondev.com>
2015-11-11 16:46:24 -0500

    Fix embedded location link

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 var tour = "Landmarks";
 var filename = filename(tour);
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   var data;
-  window.location.hash = '#' + 0;
   var tourMap = initializeMap();
   loadFile();
 
-  $(document).on('change', '#select-tour', function(){
+  $(document).on('change', '#select-tour', function () {
     filename = 'data/' + $('#select-tour').val() + '.csv';
     console.log(filename);
     window.location.hash = '#' + 0;
@@ -16,12 +15,12 @@ $(document).ready(function() {
   });
 
   function loadFile() {
-    d3.csv(filename, function(data) {
+    d3.csv(filename, function (data) {
       var tourData = loadTour(data);
       populateMap(tourMap, tourData);
       go(window.location.hash.substring(1));
 
-      $(window).on('hashchange', function() {
+      $(window).on('hashchange', function () {
         var locationNumber = parseInt(window.location.hash.substring(1));
         var currentLocation = L.latLng(tourData[locationNumber].lat, tourData[locationNumber].lng);
         tourMap.setView(currentLocation);
@@ -32,19 +31,29 @@ $(document).ready(function() {
     });
   }
 
-  $('#forward').on('click', function(e) { nextPlace(); });
-  $('#backward').on('click', function(e) { prevPlace(); });
-  $('#read-more').on('click', function(e) { expand(); });
+  $('#forward').on('click', function (e) {
+    nextPlace();
+  });
+  $('#backward').on('click', function (e) {
+    prevPlace();
+  });
+  $('#read-more').on('click', function (e) {
+    expand();
+  });
 
   function prevPlace() {
     var prev = parseInt(window.location.hash.substring(1)) - 1;
-    if (prev < 0) { prev = tourData.length - 1; }
+    if (prev < 0) {
+      prev = tourData.length - 1;
+    }
     go(prev);
   }
 
   function nextPlace() {
     var next = parseInt(window.location.hash.substring(1)) + 1;
-    if (next > tourData.length - 1) { next = 0; }
+    if (next > tourData.length - 1) {
+      next = 0;
+    }
     go(next);
   }
 });
@@ -55,16 +64,18 @@ function initializeMap() {
   var tourMap = L.map('tour-map').setView([ctrLat, ctrLng], 13);
 
   L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
   }).addTo(tourMap);
 
   return tourMap;
 }
 
 function populateMap(tourMap, tourData) {
-  var redCircleIcon = L.icon({ iconUrl: '../images/capital-red-circle-map.png',
-                               iconSize: [18, 18],
-                               className: 'locationIcon'});
+  var redCircleIcon = L.icon({
+    iconUrl: '../images/capital-red-circle-map.png',
+    iconSize: [18, 18],
+    className: 'locationIcon'
+  });
 
   tourData.map(function (d) {
     if (d['Streetview Embed Code']) {
@@ -73,7 +84,9 @@ function populateMap(tourMap, tourData) {
 
       if (lat && lng) {
         var marker = L.marker([lat, lng], {icon: redCircleIcon}).addTo(tourMap);
-        marker.on('click', function() { go(d['Index'])});
+        marker.on('click', function () {
+          go(d['Index'])
+        });
       }
     }
   });
@@ -84,9 +97,11 @@ function populateMap(tourMap, tourData) {
 function setHereMarker(tourMap, tourData) {
   var locationNumber = parseInt(window.location.hash.substring(1));
   var currentLocation = L.latLng(tourData[locationNumber].lat, tourData[locationNumber].lng);
-  var hereIcon = L.icon({ iconUrl: '../images/capital-walker.png',
-                        iconSize: [30, 30],
-                        className: 'hereIcon'});
+  var hereIcon = L.icon({
+    iconUrl: '../images/capital-walker.png',
+    iconSize: [30, 30],
+    className: 'hereIcon'
+  });
 
   L.marker(currentLocation, {icon: hereIcon, zIndexOffset: 1000}).addTo(tourMap);
 }
@@ -94,7 +109,7 @@ function setHereMarker(tourMap, tourData) {
 function loadTour(data) {
   var tourIndex = 0;
 
-  tourData = data.map(function(d) {
+  tourData = data.map(function (d) {
     d.embed = d['Streetview Embed Code'].match(/src="(.*?)"/)[1];
     d.lat = d['Streetview Embed Code'].match(/1d(.*?)!/)[1];
     d.lng = d['Streetview Embed Code'].match(/2d(.*?)!/)[1];
@@ -103,7 +118,7 @@ function loadTour(data) {
     d['Quote'] = d['Quote'].replace('\n', '<br>');
     d['Source'] = d['Source'].replace('\n', '<br>');
     d['Index'] = tourIndex;
-    tourIndex ++;
+    tourIndex++;
     return d;
   });
 
@@ -113,7 +128,7 @@ function loadTour(data) {
 function go(i) {
   if (typeof i != 'number') i = +i;
   index = i;
-  var link = "http://capital-newyork.com/index.html#" + i;
+  var link = "http://capital-newyork.com/" + fileize(tourData[index]['Tour']) + "/tour.html#" + i;
 
   $("#map").attr('src', tourData[index].embed);
   $("#tour-title").html(tourData[index]['Tour']);
@@ -147,6 +162,9 @@ function expand() {
   }
 }
 
+function fileize(title) {
+  return title.replace(/\s+/g, '-').toLowerCase();
+}
 function filename(title) {
-  return "../data/" + title.replace(/\s+/g, '-').toLowerCase() + ".csv";
+  return "../data/" + fileize(title) + ".csv";
 }
